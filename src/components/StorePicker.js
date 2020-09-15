df3e2045a52b1ba15660384d8c5676c830ed810d
Laszlo Szekely <laszlo.szekely@magicsolutions.ro>
2020-09-15 21:47:31 +0300

    11 —useRef hook Working with React Events

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React,{useRef} from 'react';
 import {getFunName} from "../helpers";
 
-export default function StorePicker() {
+export default function StorePicker(props) {
+    const storeName = useRef();
+    function goToStore(e){
+        e.preventDefault();
+        props.history.push(`store/${storeName.current.value}`);
+    }
     return (
-        <form className="store-selector">
+        <form onSubmit={goToStore} className="store-selector">
             <h2>Please Enter A Store</h2>
-            <input type="text" defaultValue={getFunName()} required placeholder="Store Name" />
+            <input ref={storeName} type="text" defaultValue={getFunName()} required placeholder="Store Name" />
             <button type="submit">Visit Store</button>
         </form>
     )
