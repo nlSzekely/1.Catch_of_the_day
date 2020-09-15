621e7796c9872a513a9814d6eb6691976b6135f2
Laszlo Szekely <laszlo.szekely@magicsolutions.ro>
2020-09-15 19:08:25 +0300

    05 — Loading CSS into React

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function StorePicker() {
     return (
-        <form className="store-picker">
-            <h2>Please Enter A Stor</h2>
+        <form className="store-selector">
+            <h2>Please Enter A Store</h2>
             <input type="text" required placeholder="Store Name" />
             <button type="submit">Visit Store</button>
         </form>
