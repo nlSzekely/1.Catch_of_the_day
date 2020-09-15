ddf979e153569c7932925b3d9a3a6411ab528e6f
Laszlo Szekely <laszlo.szekely@magicsolutions.ro>
2020-09-15 19:30:53 +0300

    07 —Header component - Passing Dynamic data with props

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,17 @@
 import React from 'react'
 
-export default function Header() {
+export default function Header(props) {
+    console.log(this)
     return (
-        <div>
-            
-        </div>
+        <header className="top">
+            <h1>
+                Catch 
+                <span className="ofThe">
+                    <span className="of">of</span>
+                    <span className="the">the</span>
+                </span>
+                day</h1>
+            <h3 className="tagline">{props.tagline}</h3>
+        </header>
     )
 }
