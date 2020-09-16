36fa3323b8ffc918ffc55175ed98e6174747af7b
Laszlo Szekely <laszlo.szekely@magicsolutions.ro>
2020-09-16 18:59:11 +0300

    14 — Loading data into state onClick - use useEffect if you want to log the new state after changed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import Header from "./components/Header";
 import Order from "./components/Order";
 import Inventory from "./components/Inventory";
-
+import sampleFishes from "./sample-fishes"
 
 function App() {
   const [fishes,setFishes] = useState({});
   const [order,setOrder] = useState({});
+  // useEffect(()=>{
+  //   console.log(fishes)
+  // },[fishes])
+
+  function loadSampleFishes(){
+    setFishes(sampleFishes);
+  }
 
   function addFish(fish){
     const fishesCopy = {...fishes};
@@ -21,7 +28,7 @@ function App() {
         <Header tagline="Fresh Seafood Market"/>
       </div>
       <Order/>
-      <Inventory addFish={addFish} />
+      <Inventory loadSampleFishes={loadSampleFishes} addFish={addFish} />
     </div>
   );
 }
--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -6,6 +6,7 @@ export default function Inventory(props) {
         <div>
             <h2>Inventory</h2>
             <AddFishForm addFish={props.addFish}/>
+            <button onClick={props.loadSampleFishes}>Load Sample Fishes</button>
         </div>
     )
 }
