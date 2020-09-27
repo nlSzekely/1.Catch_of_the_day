ccd50da431a1266e98e5c292750df1d7fd2ea908
Laszlo Szekely <laszlo.szekely@magicsolutions.ro>
2020-09-27 20:24:03 +0300

    Rebuild

---
+++ docs/precache-manifest.76d3ce85859c8f4da2cbb2448464308c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b7493500f5927080a322184b68fad6d",
+    "url": "/1.Catch_of_the_day/index.html"
+  },
+  {
+    "revision": "d121aa036df466c73a7a",
+    "url": "/1.Catch_of_the_day/static/css/main.df4efc17.chunk.css"
+  },
+  {
+    "revision": "775fadb4c6c1514ca2ab",
+    "url": "/1.Catch_of_the_day/static/js/2.f76e0c38.chunk.js"
+  },
+  {
+    "revision": "4224b32d50aca7d0b4f248bf0384ed4f",
+    "url": "/1.Catch_of_the_day/static/js/2.f76e0c38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d121aa036df466c73a7a",
+    "url": "/1.Catch_of_the_day/static/js/main.efe7fb1d.chunk.js"
+  },
+  {
+    "revision": "d5e8adf9333abe3e030a",
+    "url": "/1.Catch_of_the_day/static/js/runtime-main.d06a1431.js"
+  },
+  {
+    "revision": "d2f8799a13e6db2022d9a7de22776705",
+    "url": "/1.Catch_of_the_day/static/media/anchor.d2f8799a.svg"
+  },
+  {
+    "revision": "1461f8e9d2084c70c00830348efe1766",
+    "url": "/1.Catch_of_the_day/static/media/blanch_caps_inline-webfont.1461f8e9.woff"
+  },
+  {
+    "revision": "73a576a8254aeafebcdc66fe9fd41ac8",
+    "url": "/1.Catch_of_the_day/static/media/blanch_caps_inline-webfont.73a576a8.eot"
+  },
+  {
+    "revision": "7bbffda6bcacac8360f0ee00bb03dc04",
+    "url": "/1.Catch_of_the_day/static/media/blanch_caps_inline-webfont.7bbffda6.svg"
+  },
+  {
+    "revision": "8345e1b3362c1d640f0f11b5aae1d5bc",
+    "url": "/1.Catch_of_the_day/static/media/blanch_caps_inline-webfont.8345e1b3.ttf"
+  },
+  {
+    "revision": "4c3fb584d3f63821357c8a9cff97f360",
+    "url": "/1.Catch_of_the_day/static/media/haymaker-webfont.4c3fb584.eot"
+  },
+  {
+    "revision": "751e56274d3ea1c651ea824f902bb987",
+    "url": "/1.Catch_of_the_day/static/media/haymaker-webfont.751e5627.woff"
+  },
+  {
+    "revision": "cf798e30eda73d84b25c4ae233fe5e21",
+    "url": "/1.Catch_of_the_day/static/media/haymaker-webfont.cf798e30.svg"
+  },
+  {
+    "revision": "e9535f20ca175cc3fdfe2816f7c47168",
+    "url": "/1.Catch_of_the_day/static/media/haymaker-webfont.e9535f20.ttf"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ba6f0bf7a24ff1156762ef963a4e4492.js"
+  "/1.Catch_of_the_day/precache-manifest.76d3ce85859c8f4da2cbb2448464308c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/1.Catch_of_the_day/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
