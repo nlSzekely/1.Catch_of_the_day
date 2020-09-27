804d3279d4a3c5a3b037191c79ac1525927b42ea
Laszlo Szekely <laszlo.szekely@magicsolutions.ro>
2020-09-27 20:05:34 +0300

    Rebuild

---
+++ docs/precache-manifest.ba6f0bf7a24ff1156762ef963a4e4492.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0166982a8318ac16cc9df8924b26864",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e770826223d9b2888d29",
+    "url": "/static/css/main.9602a03d.chunk.css"
+  },
+  {
+    "revision": "2701bb865e11b9520a5b",
+    "url": "/static/js/2.ebbfd302.chunk.js"
+  },
+  {
+    "revision": "4224b32d50aca7d0b4f248bf0384ed4f",
+    "url": "/static/js/2.ebbfd302.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e770826223d9b2888d29",
+    "url": "/static/js/main.471ac67f.chunk.js"
+  },
+  {
+    "revision": "365566c80b844e7113aa",
+    "url": "/static/js/runtime-main.e517991d.js"
+  },
+  {
+    "revision": "d2f8799a13e6db2022d9a7de22776705",
+    "url": "/static/media/anchor.d2f8799a.svg"
+  },
+  {
+    "revision": "1461f8e9d2084c70c00830348efe1766",
+    "url": "/static/media/blanch_caps_inline-webfont.1461f8e9.woff"
+  },
+  {
+    "revision": "73a576a8254aeafebcdc66fe9fd41ac8",
+    "url": "/static/media/blanch_caps_inline-webfont.73a576a8.eot"
+  },
+  {
+    "revision": "7bbffda6bcacac8360f0ee00bb03dc04",
+    "url": "/static/media/blanch_caps_inline-webfont.7bbffda6.svg"
+  },
+  {
+    "revision": "8345e1b3362c1d640f0f11b5aae1d5bc",
+    "url": "/static/media/blanch_caps_inline-webfont.8345e1b3.ttf"
+  },
+  {
+    "revision": "4c3fb584d3f63821357c8a9cff97f360",
+    "url": "/static/media/haymaker-webfont.4c3fb584.eot"
+  },
+  {
+    "revision": "751e56274d3ea1c651ea824f902bb987",
+    "url": "/static/media/haymaker-webfont.751e5627.woff"
+  },
+  {
+    "revision": "cf798e30eda73d84b25c4ae233fe5e21",
+    "url": "/static/media/haymaker-webfont.cf798e30.svg"
+  },
+  {
+    "revision": "e9535f20ca175cc3fdfe2816f7c47168",
+    "url": "/static/media/haymaker-webfont.e9535f20.ttf"
+  }
+]);
